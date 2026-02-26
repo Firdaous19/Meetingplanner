e8a582ab25fb7fc9aaeef6db7f0659d598a9f30d
firdaous <firdaous.abzik@gmail.com>
2026-02-26 16:43:13 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "tinyxml/tinyxml.h"
+int main() {
+    TiXmlDocument doc;
+    if (!doc.LoadFile("een CD.xml")) {
+        std::cerr << doc.ErrorDesc() << std::endl;
+        return 1;
+    }
+    TiXmlElement *root = doc.FirstChildElement();
+    if (root == NULL) {
+        std::cerr << "Failed to load file: No root element." << std::endl;
+        doc.Clear();
+        return 1;
+    }
+    for (TiXmlElement *elem = root->FirstChildElement();
+         elem != NULL;
+         elem = elem->NextSiblingElement()) {
+
+        std::string name = elem->Value();
+
+        TiXmlNode *child = elem->FirstChild();
+        if (!child) continue;
+
+        TiXmlText *text = child->ToText();
+        if (!text) continue;
+
+        std::string value = text->Value();
+
+        if (name == "ARTIST" || name == "Artist")
+            std::cout << "Artist: " << value << std::endl;
+
+        if (name == "TITLE" || name == "Title")
+            std::cout << "Title: " << value << std::endl;
+    }
+
+    doc.Clear();
+    return 0;
+}
