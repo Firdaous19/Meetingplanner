fb32abef28e3e9ac360a25ed5f1237ab41939efb
ali_s <sinem.alieva@student.uantwerpen.be>
2026-05-08 02:56:11 +0200

    commentaar toegevoegd aan main.cpp

--- week2_code/main.cpp
+++ week2_code/main.cpp
@@ -7,11 +7,11 @@ int main() {
     const std::string inputFile = "../week2_code/test_laatste.xml";
     const std::string outputFile = "output.txt";
 
-    MeetingPlanner planner;
-    XMLParser parser;
-    OutputWriter writer;
+    MeetingPlanner planner;      // planner bewaart en verwerkt alle data van het systeem//
+    XMLParser parser;   //parser leest input//
+    OutputWriter writer;   //object writer schrijft het resultaat naar een tekstbestand//
 
-    bool success = parser.parse(inputFile, planner);
+    bool success = parser.parse(inputFile, planner);   //parser gaat de XMLbestand van de inputfile openen en lezen. maakt dan objecten aan en voegt die toe aan planner//
 
     if (!success) {
         std::cerr << "Inlezen van XML is mislukt." << std::endl;
@@ -23,8 +23,8 @@ int main() {
         return 1;
     }
 
-    planner.processMeetings();
-    writer.writeOutput(outputFile, planner);
+    planner.processMeetings();     //het object planner gaat de meetings verwerken; vb controleren of een room bezet/in renovatie is , occupancy/CO2/cateringkost berekenen, successfull meetings bijhouden//
+    writer.writeOutput(outputFile, planner);   // object writer gaat wat in de planner staat(meetings,rroms,conflicts,..) overschrijven in de outputfile output.txt//
 
     std::cout << "Aantal rooms geladen: " << planner.getRooms().size() << std::endl;
     std::cout << "Aantal meetings geladen: " << planner.getMeetings().size() << std::endl;
